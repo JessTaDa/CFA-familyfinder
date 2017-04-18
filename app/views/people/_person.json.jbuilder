7a1c045da4643f008f0a12463bde8878365bb907
Jesstada <jesstada@gmail.com>
2017-04-18 19:01:42 +1000

    scaffold Person model

---
+++ app/views/people/_person.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! person, :id, :user_id, :relation, :name, :age, :town, :story, :missing, :created_at, :updated_at
+json.url person_url(person, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418084841) do
+ActiveRecord::Schema.define(version: 20170418090051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "relation"
+    t.string   "name"
+    t.integer  "age"
+    t.string   "town"
+    t.text     "story"
+    t.boolean  "missing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_people_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 20170418084841) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "people", "users"
 end
