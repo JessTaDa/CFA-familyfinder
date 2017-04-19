0c9c5169956dea043dc87a9c138a2fa43fcbe9fa
Jesstada <jesstada@gmail.com>
2017-04-19 11:27:09 +1000

    devise redirect proper method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
 # redirects signed in user to user profile
-  authenticated :user do
-    root 'pages#profile', as: :authenticated_person
-  end
+  # authenticated :user do
+  #   root 'pages#profile', as: :authenticated_person
+  # end
 
   get 'pages/home'
 
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   resources :people
   resources :registrations
 
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users#, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
