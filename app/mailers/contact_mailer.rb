4bd7446d1188a96c30e5f52e6579f9c851e52b9b
Jesstada <jesstada@gmail.com>
2017-04-25 12:18:57 +1000

    simpleform setup

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,11 @@
 class ContactMailer < ApplicationMailer
-
-  default from: 'admin@familyfinder.com'
+default from: 'admin@familyfinder.com'
   def send_contact_email(email, message)
     @email = email
     @message = message
-    mail(to: ENV["TADA_EMAIL"],
-             subject: 'New message from website')
-       do |format|
-          format.html { render 'contact_mailer' }
-          format.text { render 'contact_mailer' }
-      end
+    mail(to: ENV["TADA_EMAIL"], subject: 'New message from website') do |format|
+          format.html { render 'send_contact_email' }
+          format.text { render 'send_contact_email' }
+    end
   end
-
 end
